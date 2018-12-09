97601d05b66374576e5da72953199a3afeec3383
Eka Wijaya Susilo <ekawijayasusilo@gmail.com>
2018-12-09 20:44:47 +0700

    Update scriptrunner.py

--- scriptrunner.py
+++ scriptrunner.py
@@ -5,17 +5,20 @@ import boto3
 cmdDict={
     'python3':'python3 $(SCRIPT_NAME) $(ARGS)',
 }
-RUNTIME = os.environ['RUNTIME']
-SCRIPT_NAME = os.environ['SCRIPT_NAME']
 
-taskList = [line.strip() for line in open("tasklist.txt", 'r')]
-if len(taskList)>0:
-    command = cmdDict[RUNTIME]
-    command = command.replace('$(SCRIPT_NAME)', SCRIPT_NAME)
-    command = command.replace('$(ARGS)', taskList[0])
-    call(command, shell=True)
+try:
+    RUNTIME = os.environ['RUNTIME']
+    SCRIPT_NAME = os.environ['SCRIPT_NAME']
+    
     taskList = [line.strip() for line in open("tasklist.txt", 'r')]
-    with open('tasklist.txt', 'w') as f:
-        for line in taskList[1:]:
-            f.write(line+'\n')
-
+    if len(taskList)>0:
+        command = cmdDict[RUNTIME]
+        command = command.replace('$(SCRIPT_NAME)', SCRIPT_NAME)
+        command = command.replace('$(ARGS)', taskList[0])
+        call(command, shell=True)
+        taskList = [line.strip() for line in open("tasklist.txt", 'r')]
+        with open('tasklist.txt', 'w') as f:
+            for line in taskList[1:]:
+                f.write(line+'\n')
+except:
+    pass
