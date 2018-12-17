ca50436e7804b7826260680828763c5a4c38901d
Eka Wijaya Susilo <ekawijayasusilo@gmail.com>
2018-12-17 10:17:50 +0700

    Update scriptrunner.py

--- scriptrunner.py
+++ scriptrunner.py
@@ -5,6 +5,12 @@ import boto3
 cmdDict={
     'python3':'python3 $(SCRIPT_NAME) $(ARGS)',
 }
+INSTANCE_ID = os.environ['INSTANCE_ID']
+RESULT_BUCKET_NAME = os.environ['RESULT_BUCKET_NAME']
+DATA_BUCKET_NAME = os.environ['DATA_BUCKET_NAME']
+
+s3Resource = boto3.resource('s3')
+bucket = s3Resource.Bucket(DATA_BUCKET_NAME)
 
 try:
     RUNTIME = os.environ['RUNTIME']
@@ -12,13 +18,22 @@ try:
     
     taskList = [line.strip() for line in open("tasklist.txt", 'r')]
     if len(taskList)>0:
-        command = cmdDict[RUNTIME]
-        command = command.replace('$(SCRIPT_NAME)', SCRIPT_NAME)
-        command = command.replace('$(ARGS)', taskList[0])
-        call(command, shell=True)
-        taskList = [line.strip() for line in open("tasklist.txt", 'r')]
+        taskListUpdated = [line.strip() for line in open("tasklist.txt", 'r')]
         with open('tasklist.txt', 'w') as f:
             for line in taskList[1:]:
                 f.write(line+'\n')
+        objFileName=taskList[0]
+        bucket.download_file(objFileName, objFileName)
+        command = cmdDict[RUNTIME]
+        command = command.replace('$(SCRIPT_NAME)', SCRIPT_NAME)
+        command = command.replace('$(ARGS)', objFileName)
+        call(command, shell=True)
+        s3Resource.Bucket(RESULT_BUCKET_NAME).upload_file(objFileName, objFileName+'_by_'+INSTANCE_ID)
+        objAcl = s3Resource.ObjectAcl(RESULT_BUCKET_NAME,objFileName+'_by_'+INSTANCE_ID)
+        objAcl.put(ACL='public-read')
 except:
     pass
+
+
+
+
